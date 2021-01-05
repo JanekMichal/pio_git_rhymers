21c39ea419833f02e5575f3f739a3eb539a349a2
Klaudiusz <klaudiusz.boblack@gmail.com>
2021-01-05 11:36:37 +0100

    make final variable extract const

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,12 +2,15 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
-    int totalRejcted = 0;
+    public static final int INT2 = 0;
+
+    int totalRejcted = INT2;
 
     public int getTotalRejcted() {
         return totalRejcted;
     }
 
+    @Override
     public void countIn(int in) {
         if (!callCheck() && in > getLastNumber()) {
             totalRejcted++;
